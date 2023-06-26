502c5252d848b4bfd0ee797c72b983c5fa873789
Rafi gunawan <figun746@gmail.com>
2023-06-26 20:57:31 +0700

    update placholder filter

--- src/components/FilterComponent.tsx
+++ src/components/FilterComponent.tsx
@@ -1,5 +1,5 @@
 import { Close } from "@icon-park/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 type FilterComponentProps = {
   filterText: string;
@@ -13,19 +13,31 @@ export default function FilterComponent({
   onClear,
 }: FilterComponentProps) {
   const [focus, setFocus] = useState<boolean>(false);
+  const ref = useRef<HTMLInputElement>(null);
 
   return (
     <div className={`mb-5 border-2 rounded-lg flex`}>
       <input
-        className={`px-4 py-1 rounded-lg outline-none ${
+        className={`px-4 py-1 rounded-lg bg-none outline-none ${
           focus ? "border-emerald-500" : "border-gray-300"
         }`}
         type="date"
+        ref={ref}
         placeholder="Filter By Date"
         value={filterText}
         onChange={(e) => onFilter(e.target.value)}
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
+        onFocus={() => {
+          setFocus(true);
+          if (ref.current) {
+            ref.current.type = "date";
+          }
+        }}
+        onBlur={() => {
+          setFocus(false);
+          if (ref.current) {
+            ref.current.type = "text";
+          }
+        }}
       />
       <button
         className="bg-emerald-200 rounded-r-md border-2 hover:border-emerald-600 border-emerald-200 hover:bg-emerald-600 right-0 inline-block px-2 py-1 h-full aspect-square font-semibold uppercase text-white"
