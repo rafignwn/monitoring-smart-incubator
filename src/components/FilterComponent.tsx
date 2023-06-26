858cde14c481165914e3ec682ddb83d0ac7a07ea
Rafi gunawan <figun746@gmail.com>
2023-06-26 22:13:13 +0700

    update tampilan filter

--- src/components/FilterComponent.tsx
+++ src/components/FilterComponent.tsx
@@ -15,30 +15,37 @@ export default function FilterComponent({
   const [focus, setFocus] = useState<boolean>(false);
   const ref = useRef<HTMLInputElement>(null);
 
+  function openDatePicker() {
+    if (ref.current) {
+      ref.current.showPicker();
+    }
+  }
+
   return (
-    <div className={`mb-5 border-2 rounded-lg flex`}>
-      <input
-        className={`px-4 py-1 rounded-lg bg-none outline-none ${
-          focus ? "border-emerald-500" : "border-gray-300"
-        }`}
-        type="date"
-        ref={ref}
-        placeholder="Filter By Date"
-        value={filterText}
-        onChange={(e) => onFilter(e.target.value)}
-        onFocus={() => {
-          setFocus(true);
-          if (ref.current) {
-            ref.current.type = "date";
-          }
-        }}
-        onBlur={() => {
-          setFocus(false);
-          if (ref.current) {
-            ref.current.type = "text";
-          }
-        }}
-      />
+    <div
+      className={`mb-5 border-2 rounded-lg flex ${
+        focus ? "border-emerald-500" : "border-emerald-100"
+      }`}
+    >
+      <label
+        className={`px-4 w-40 text-left py-2 relative rounded-lg text-gray-500 outline-none`}
+        htmlFor="FilterInput"
+        onClick={openDatePicker}
+      >
+        {filterText ? filterText : "Filter By Date"}
+
+        <input
+          className="absolute w-0 h-0 top-0 left-0"
+          type="date"
+          id="FilterInput"
+          ref={ref}
+          placeholder="Filter By Date"
+          value={filterText}
+          onChange={(e) => onFilter(e.target.value)}
+          onFocus={() => setFocus(true)}
+          onBlur={() => setFocus(false)}
+        />
+      </label>
       <button
         className="bg-emerald-200 rounded-r-md border-2 hover:border-emerald-600 border-emerald-200 hover:bg-emerald-600 right-0 inline-block px-2 py-1 h-full aspect-square font-semibold uppercase text-white"
         onClick={onClear}
